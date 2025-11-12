f0d481378f1297e82f289caafbd8fd28a8d210f2
HaiqaK501 <l250818@lhr.nu.edu.pk>
2025-11-12 20:59:45 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include "functions.h"
+using namespace std;
+
+int main() {
+	srand(time(0));
+	cout << "------Candy Crush Saga---------" << endl;
+	show_message(0);
+}
